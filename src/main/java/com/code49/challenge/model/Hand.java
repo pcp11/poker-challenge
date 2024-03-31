3b1e0f18c7414ef9c8f401e748b2988676759fa4
Patrick Christ <patrick.christ.91@web.de>
2024-03-31 12:54:09 +0200

    implement hand model

---
+++ src/main/java/com/code49/challenge/model/Hand.java
@@ -0,0 +1,21 @@
+package com.code49.challenge.model;
+
+import java.util.List;
+
+import static com.code49.challenge.model.HandRank.HIGH_CARD;
+import static java.util.Comparator.comparing;
+
+public record Hand(List<Card> cards, HandRank rank) implements Comparable<Hand> {
+
+    public static Hand from(List<Card> cards) {
+        if (cards == null || cards.size() != 5) {
+            throw new IllegalArgumentException("A poker hand requires 5 cards.");
+        }
+        return new Hand(cards, HIGH_CARD);
+    }
+
+    @Override
+    public int compareTo(Hand other) {
+        return comparing(Hand::rank).compare(this, other);
+    }
+}
---
+++ src/main/java/com/code49/challenge/model/HandRank.java
@@ -0,0 +1,13 @@
+package com.code49.challenge.model;
+
+public enum HandRank {
+    HIGH_CARD,
+    ONE_PAIR,
+    TWO_PAIRS,
+    THREE_OF_A_KIND,
+    STRAIGHT,
+    FLUSH,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    STRAIGHT_FLUSH
+}
