0c815957742c4ba0f111c25c8fb88be05dd5962e
Patrick Christ <patrick.christ.91@web.de>
2024-04-01 16:38:55 +0200

    remove unnecessary compareTo method

--- src/main/java/com/code49/challenge/model/Card.java
+++ src/main/java/com/code49/challenge/model/Card.java
@@ -1,12 +1,3 @@
 package com.code49.challenge.model;
 
-import static java.util.Comparator.comparing;
-
-public record Card(CardSuit suit, CardValue value) implements Comparable<Card> {
-
-    @Override
-    public int compareTo(Card other) {
-        return comparing(Card::value)
-                .compare(this, other);
-    }
-}
+public record Card(CardSuit suit, CardValue value) {}
